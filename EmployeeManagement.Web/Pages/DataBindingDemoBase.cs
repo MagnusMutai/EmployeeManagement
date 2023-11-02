110d51e8ab063e8df885d42ea2acd491a8156005
Gilly <mutaigilly02@gmail.com>
2023-11-02 14:11:51 +0300

    Millions of Commits

---
+++ EmployeeManagement.Web/Pages/DataBindingDemoBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DataBindingDemoBase : ComponentBase
+    {
+        public string Name { get; set; } = "Tom";
+    }
+}
