de29f3136e701597ad5cb25074a8194152fefb81
Gilly <MagnusMq07@gmail.com>
2023-11-15 14:32:34 +0300

    Let's leave it at that

---
+++ EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
@@ -0,0 +1,10 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DisplayEmployeeBase : ComponentBase
+    {
+        public Employee Employee { get; set; }
+    }
+}
