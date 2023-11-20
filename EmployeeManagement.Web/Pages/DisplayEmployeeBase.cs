b4d72c8ebd1ba521ee35025dee230f82c61cc0bd
Gilly <MagnusMq07@gmail.com>
2023-11-20 15:05:13 +0300

    Commit this thing

--- EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
+++ EmployeeManagement.Web/Pages/DisplayEmployeeBase.cs
@@ -6,9 +6,17 @@ namespace EmployeeManagement.Web.Pages
     public class DisplayEmployeeBase : ComponentBase
     {
         [Parameter]
-        public IEnumerable<Employee> Employee { get; set; }
+        public Employee Employee { get; set; }
 
         [Parameter]
         public bool ShowFooter { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> OnEmployeeSelection { get; set; }
+
+        protected async Task CheckBoxChanged(ChangeEventArgs e)
+        {
+            await OnEmployeeSelection.InvokeAsync((bool)e.Value);
+        }
     }
 }
