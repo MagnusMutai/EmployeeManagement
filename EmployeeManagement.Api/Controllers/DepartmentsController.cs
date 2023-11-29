f55e003f15c0b4c8c0df4634434aaa2a37c001aa
Gilly <MagnusMq07@gmail.com>
2023-11-29 14:03:49 +0300

    Commit this chunk

---
+++ EmployeeManagement.Api/Controllers/DepartmentsController.cs
@@ -0,0 +1,57 @@
+﻿using EmployeeManagement.Api.Models;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly IDepartmentRepository departmentRepository;
+
+        public DepartmentsController(IDepartmentRepository departmentRepository)
+        {
+            this.departmentRepository = departmentRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetDepartments()
+        {
+
+            try
+            {
+                return Ok(await departmentRepository.GetDepartments());
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from database");
+            }
+
+        }
+
+        [HttpGet("{int:id}")]
+        public async Task<ActionResult<Department>> GetDepartment(int id)
+        {
+            try
+            {
+                var result = await departmentRepository.GetDepartment(id);
+
+                if(result == null)
+                {
+                    return NotFound();
+                }
+
+                return result;
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retreiving data from the database.");
+            }
+        }
+    }
+}
