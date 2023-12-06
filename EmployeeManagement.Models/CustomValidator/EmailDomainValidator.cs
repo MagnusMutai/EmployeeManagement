ec562dcdafd6002102459d71504c6a5459d81de0
Gilly <MagnusMq07@gmail.com>
2023-12-06 10:45:08 +0300

    Commit da ting

---
+++ EmployeeManagement.Models/CustomValidator/EmailDomainValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models.CustomValidator
+{
+    public class EmailDomainValidator : ValidationAttribute
+    {
+        public string AllowedDomain { get; set; }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            string[] strings = value.ToString().Split('@');
+            if (strings[1].ToUpper() == AllowedDomain.ToUpper())
+            {
+                return null;
+            }
+
+            return new ValidationResult(ErrorMessage,
+            new[] { validationContext.MemberName });
+        }
+
+    }
+}
--- EmployeeManagement.Models/Employee.cs
+++ EmployeeManagement.Models/Employee.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeManagement.Models.CustomValidator;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -10,15 +11,17 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         public int EmployeeId { get; set; }
-        [Required]
+        [Required (ErrorMessage = "Please input First Name")]
         [MinLength(2)]
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
+        [EmailAddress]
+        [EmailDomainValidator(AllowedDomain ="MagnusMq.com", ErrorMessage ="Error message validation")]
         public string Email { get; set; }
         public DateTime DateOfBirth { get; set; }
         public Gender Gender { get; set; }
-        public int DepartmentId { get; set; }
+        public int DepartmentId { get; set; } 
         public string PhotoPath { get; set; }
         //navigation property
         public Department Department { get; set; }
