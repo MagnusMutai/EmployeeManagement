add398544e48b2114de97159d046acf7ebf2fe2a
Gilly <MagnusMq07@gmail.com>
2023-12-06 15:55:05 +0300

    Commit this

--- EmployeeManagement.Models/CustomValidator/EmailDomainValidator.cs
+++ EmployeeManagement.Models/CustomValidator/EmailDomainValidator.cs
@@ -12,14 +12,18 @@ namespace EmployeeManagement.Models.CustomValidator
         public string AllowedDomain { get; set; }
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
-            string[] strings = value.ToString().Split('@');
-            if (strings.Length > 1 && strings[1].ToUpper() == AllowedDomain.ToUpper())
+            if (value != null)
             {
-                return null;
-            }
+                string[] strings = value.ToString().Split('@');
+                if (strings.Length > 1 && strings[1].ToUpper() == AllowedDomain.ToUpper())
+                {
+                    return null;
+                }
 
-            return new ValidationResult(ErrorMessage,
-            new[] { validationContext.MemberName });
+                return new ValidationResult(ErrorMessage,
+                new[] { validationContext.MemberName });
+            }
+            return null;
         }
 
     }
