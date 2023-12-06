f2a69fcc10bf209ca3fd85982f758e7c7bde7d11
Gilly <MagnusMq07@gmail.com>
2023-12-06 16:10:43 +0300

    commit this again

--- EmployeeManagement.Models/CustomValidator/EmailDomainValidator.cs
+++ EmployeeManagement.Models/CustomValidator/EmailDomainValidator.cs
@@ -12,18 +12,14 @@ namespace EmployeeManagement.Models.CustomValidator
         public string AllowedDomain { get; set; }
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
-            if (value != null)
+            string[] strings = value.ToString().Split('@');
+            if (strings.Length > 1 && strings[1].ToUpper() == AllowedDomain.ToUpper())
             {
-                string[] strings = value.ToString().Split('@');
-                if (strings.Length > 1 && strings[1].ToUpper() == AllowedDomain.ToUpper())
-                {
-                    return null;
-                }
-
-                return new ValidationResult(ErrorMessage,
-                new[] { validationContext.MemberName });
+                return null;
             }
-            return null;
+
+            return new ValidationResult(ErrorMessage,
+            new[] { validationContext.MemberName });
         }
 
     }
